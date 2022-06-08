7be27c9e75f764e3e84004cc16c76ba61c95faef
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-06-08 18:19:01 -0400

    gg

---
+++ src/GamePanel.java
@@ -0,0 +1,55 @@
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
+import java.util.Random;
+import java.awt.event.KeyAdapter;
+
+
+
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH =600;
+    static final int SCREEN_HEIGHT =600;
+
+
+    GamePanel(){
+
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver( Graphics g) {
+
+    }
+    public class MyKeyAdapter extends keyAdapter{
+        public void KeyPressed(KeyEvent e) {
+
+        }
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("santosh is james  ");
+    public static void main(String[]args){
+
+
+        }
 
     }
-}
+
